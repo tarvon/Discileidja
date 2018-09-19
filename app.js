a75c90e9fbaf1957d276ca869dcdf22ca95e1915
Tarvo Nippolainen <tarvo.nippolainen@gmail.com>
2018-09-19 16:52:22 +0300

    veits kokkutõmmatud

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use('/', indexRouter);
+
+module.exports = app;
+
+/*
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +46,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
 module.exports = router;
+
+
