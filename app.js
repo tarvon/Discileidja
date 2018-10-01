fe1fe285da47aeff585e9848d315885f64d09eb6
Tarvo Nippolainen <tarvo.nippolainen@gmail.com>
2018-10-01 11:50:57 +0300

    parandus

--- app.js
+++ app.js
@@ -34,38 +34,7 @@ server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "db-discileidja.cvtuddwcibzq.eu-central-1.rds.amazonaws.com",
-    user: "root",
-    password: "kettaleidjaandmebaas",
-    database: "ebdb"
-});
-
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-    con.connect();
-    console.log("Connected!");
-    var sql = "INSERT INTO users (id, eesnimi, perenimi, profiilipilturl, meiliaadress)" +
-        " VALUES ('+profile.getId()+','+profile.getGivenName()+','+profile.getFamilyName()+'," +
-        "'+profile.getImageUrl()+','+profile.getEmail()+' ')";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-    });
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-}
 
 
 /*
