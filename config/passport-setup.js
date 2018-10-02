61ebff491e4c919fcab6f4ab210ba773b6d69d9f
Tarvo Nippolainen <tarvo.nippolainen@gmail.com>
2018-10-02 18:05:09 +0300

    Serialize

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -9,13 +9,17 @@ passport.serializeUser((user, done)=>{
     done(null, user.id);
 });
 
+passport.deserializeUser((id, done)=>{
+    User.findById
+    done(null, user.id);
+});
+
 passport.use(new GoogleStrategy({
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
         callbackURL: "/auth/google/redirect"
     },
     function(accessToken, refreshToken, profile, done) {
-        //check if user exists
 
         let connection = mysql.createConnection({
             host: keys.AWSRDS.host,
@@ -28,24 +32,50 @@ passport.use(new GoogleStrategy({
             if (err) {
                 return console.error('error: ' + err.message);
             }
-
             console.log('Connected to the MySQL server.');
+
             let query1 = profile.id;
             let query2 = profile.displayName;
             let query3 = profile.name.givenName;
             let query4 = profile.name.familyName;
             let query5 = profile.emails[0].value;
 
-            let sql = "INSERT INTO users(GoogleID,FullName,GivenName,FamilyName,Email) VALUES ('"+query1+"','"+query2+"','"+query3+"','"+query4+"','"+query5+"')";
 
-            connection.query(sql, (err, results) =>  {
-                if (err) {
-                    return console.error(err.message);
+            let sqlCurrentUser = "SELECT * FROM users WHERE GoogleID=?";
+            let sqlCreatedUser = "SELECT * FROM users WHERE Id=?";
+            let sqlNewUser = "INSERT INTO users(GoogleID,FullName,GivenName,FamilyName,Email) VALUES ('"+query1+"','"+query2+"','"+query3+"','"+query4+"','"+query5+"')";
+
+
+            //check if user exists
+            connection.query(sqlCurrentUser, profile.id, (error, currentUser, fields) => {
+                if (error) {
+                    return console.error(error.message);
                 }
-                console.log('Todo ID:' + results.insertId);
+                if (currentUser.length > 0){
+                    console.log("User is: ", currentUser)
+                    done(null, currentUser);
+                } else {
 
+                    //create new user
+                    connection.query(sqlNewUser, (err, newUser) =>  {
+                        if (err) {
+                            return console.error(err.message);
+                        }
+                        console.log('New user ID:' + newUser.insertId);
+                        let CreatedUserDbId = newUser.insertId;
+
+                        //find created user
+                        connection.query(sqlCreatedUser, CreatedUserDbId,  (err, CreatedUser, fields) =>  {
+                            if (err) {
+                                return console.error(err.message);
+                            }
+                            console.log('Created user',  CreatedUser);
+                            done(null, CreatedUser);
+                        });
+
+                    });
+                }
             });
-            connection.end();
         });
     }
 ));
