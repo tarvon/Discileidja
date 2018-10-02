9c8664f0c0e3c12153b9c6ac132c542f1456b487
Tarvo Nippolainen <tarvo.nippolainen@gmail.com>
2018-10-02 21:13:45 +0300

    port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 
 const hostname = '127.0.0.1';
-const port = 3000; //aws: 8081 ; local: 3000
+const port = 8081; //aws: 8081 ; local: 3000
 
 var authRouter = require('./routes/auth-routes');
 var homeRouter = require('./routes/home');
