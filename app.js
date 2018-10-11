8487ce33f4dd87bb76cf3a6d4ee1f1c5bd699c69
Silver <silvermaala@hotmail.com>
2018-10-11 23:53:24 +0300

    Tõstsin postis kasutatud meetodi /lib/leidsin.js, andmebaasi päring toimub keys.js failis olevate andmetega, tegin selle faili rename'imise korda.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const multer = require('multer');
 
 const hostname = '127.0.0.1';
 const port = 8081; //aws: 8081 ; local: 3000
@@ -20,6 +21,7 @@ var abiRouter = require('./routes/abi');
 var leidsinRouter = require('./routes/leidsin');
 var otsinRouter = require('./routes/otsin');
 var profiilRouter = require('./routes/profiil');
+var leidsin = require('./lib/leidsin');
 
 var app = express();
 
@@ -57,46 +59,35 @@ server.listen(port, hostname, () => {
 
 /* post method on leidsin.html */
 app.post('/leidsin', (req, res) => {
-    leidsinRouter.addDB(req, res);
-    res.send('Töötab');
+    leidsin.addDB(req, res);
     res.end();
 });
 
 
 
 /*
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
-
 */
