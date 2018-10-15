3bb86769c7c9c2baec316ad140f4750e457a38b8
Tarvo Nippolainen <tarvo.nippolainen@gmail.com>
2018-10-15 14:25:14 +0300

    HTML JS cleanup

---
+++ public/js/CDNfallback.js
@@ -0,0 +1,29 @@
+console.log("kurwa");
+
+fallback.load({
+    JSON: '//cdnjs.cloudflare.com/ajax/libs/json2/20121008/json2.min.js',
+    BootstrapCSS: [
+        '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.cs'
+    ],
+    Bootstrap: [
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+        '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js'
+    ],
+    jQuery: [
+        '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js',
+        '//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js'
+    ],
+    'jQuery.ui': [
+        '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+        '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+        '//js/loader.js?i=vendor/jquery-ui.min.js'
+    ],
+    Popper: [
+        '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'
+    ],
+}, {
+    shim: {
+        'jQuery.ui': ['jQuery']
+    },
+});
