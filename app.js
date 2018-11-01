4fba01a64ce36c45041d7db0cb99ee2f55982066
Tarvo Nippolainen <tarvo.nippolainen@gmail.com>
2018-11-01 11:37:18 +0200

    test

--- app.js
+++ app.js
@@ -97,7 +97,10 @@ app.use(function (req, res, next) {
     let currentBrowser = browserDetectResult.name;
     let currentos = browserDetectResult.os;
 
-    let ipa = req.header('x-forwarded-for').split(",").pop() || req.connection.remoteAddress;
+    let ipa = (req.headers['x-forwarded-for'] || '').split(',').pop() ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress
 
     console.log(ipa);
 
